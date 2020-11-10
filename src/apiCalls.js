478fe1ba0e80d9a3632f114c96ef053d0e6178da
Orlando Murcio <ato.murcio@gmail.com>
2020-11-10 10:38:34 -0700

    Write method to post to APi

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,3 +7,38 @@ export const getUrls = async() => {
     return response.error
   }
 }
+
+export const sendUrls = async(url) => {
+  let int = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(url)
+  }
+  const response= await fetch('http://localhost:3001/api/v1/urls', int)
+  if(response.ok){
+    return await response.json()
+  } else{
+    console.log(response)
+    return response.error
+  }
+}
+
+// myHeaders = new Headers();
+// myHeaders.append("Content-Type", "application/json");
+
+// var raw = JSON.stringify({"long_url":"long_url","title":"title"});
+
+// var requestOptions = {
+//   method: 'POST',
+//   headers: myHeaders,
+//   body: raw,
+//   redirect: 'follow'
+// };
+
+// fetch("http://localhost:3001/api/v1/urls", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+
