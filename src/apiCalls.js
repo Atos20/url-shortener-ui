2ffcce844a6171a975689dbd59934fe1004d704a
Orlando Murcio <ato.murcio@gmail.com>
2020-11-10 09:31:42 -0700

    Add logic to get data from API

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,9 @@
-export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+export const getUrls = async() => {
+  const response= await fetch('http://localhost:3001/api/v1/urls')
+  if(response.ok){
+    return await response.json()
+  } else{
+    console.log(response)
+    return response.error
+  }
 }
